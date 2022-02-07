9702bb41c1bd749085753346b65974840c467dce
Erik Eelde <erik.eelde@gmail.com>
2022-02-08 00:07:19 +0100

    Validate check fails on invalid toml of failing rules

---
+++ src/test/fixtures/singlemodule_fail/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("se.premex.ownership")
+}
+
+ownership {
+    validateOwnership = true
+}
+
+abstract class CheckTask : DefaultTask() {
+    @TaskAction
+    fun greet() {
+        println("fake check task")
+    }
+}
+
+// Create a task using the task type
+tasks.register<CheckTask>("check")
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
