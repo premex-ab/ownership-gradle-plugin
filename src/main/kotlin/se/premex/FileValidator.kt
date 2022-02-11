152bea6dd01d36e956617508e7476dcb28f929d7
warting <stefan@warting.se>
2022-02-11 13:12:56 +0100

    replace tomlj with jackson

--- src/main/kotlin/se/premex/FileValidator.kt
+++ src/main/kotlin/se/premex/FileValidator.kt
@@ -1,40 +1,38 @@
 package se.premex
 
-import org.tomlj.Toml
-import org.tomlj.TomlParseResult
-import org.tomlj.TomlVersion
+import se.premex.toml.OwnershipFileResult
 import java.io.File
 
 sealed class Rule {
-    data class DottedKeyExists(val property: String) : Rule()
+    object OwnerExists : Rule()
     object ValidVersion : Rule()
 }
 
 data class ValidationRules(val validationRules: List<Rule>)
 data class OwnershipValidationResult(val results: List<Pair<Rule, Boolean>>)
 data class ValidationResult(
-    val tomlParseResult: TomlParseResult,
+    val tomlParseResult: OwnershipFileResult,
+    val hasError: Boolean,
     val ownershipValidationResult: OwnershipValidationResult?
 )
 
-val TOML_VERSION = TomlVersion.V0_5_0
 val VALID_VERSIONS = arrayOf(1L)
 
 class FileValidator {
 
     internal val defaultChecks = ValidationRules(
         listOf(
-            Rule.DottedKeyExists(property = "owner"),
+            Rule.OwnerExists,
             Rule.ValidVersion,
         )
     )
 
     internal fun validateOwnership(tomlFile: File): OwnershipValidationResult =
-        validateOwnership(defaultChecks, Toml.parse(tomlFile.reader(), TOML_VERSION))
+        validateOwnership(defaultChecks, TomlParser.parseFile(tomlFile))
 
     internal fun validateOwnership(
         validationRules: ValidationRules,
-        tomlParseResult: TomlParseResult
+        tomlParseResult: OwnershipFileResult
     ): OwnershipValidationResult {
         val results = validationRules.validationRules.map {
             validateRule(tomlParseResult, it)
@@ -42,10 +40,10 @@ class FileValidator {
         return OwnershipValidationResult(results)
     }
 
-    private fun validateRule(tomlParseResult: TomlParseResult, rule: Rule): Pair<Rule, Boolean> = when (rule) {
-        is Rule.DottedKeyExists -> {
-            rule to tomlParseResult.contains(rule.property)
+    private fun validateRule(tomlParseResult: OwnershipFileResult, rule: Rule): Pair<Rule, Boolean> = when (rule) {
+        is Rule.OwnerExists -> {
+            rule to (!tomlParseResult.ownershipFile?.owner?.user.isNullOrEmpty())
         }
-        is Rule.ValidVersion -> rule to VALID_VERSIONS.contains(tomlParseResult.getLong("version"))
+        is Rule.ValidVersion -> rule to VALID_VERSIONS.contains(tomlParseResult.ownershipFile?.version ?: 0)
     }
 }
